82eb12a09ad4a25bb46ec90e1699674622bf02bc
Fan <fanfeng2015@gmail.com>
2017-02-01 22:15:06 -0500

    #89

---
+++ GrayCode/src/grayCode/GrayCode.java
@@ -0,0 +1,19 @@
+package grayCode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+// LeetCode #89
+// The gray code is a binary numeral system where two successive values differ in only one bit.
+// Given a non-negative integer n, representing the total number of bits in the code, print the sequence of gray code. 
+// A gray code sequence must begin with 0.
+
+public class GrayCode {
+
+	public List<Integer> grayCode(int n) {
+		List<Integer> result = new LinkedList<>();
+		for (int i = 0; i < (1 << n); i++)
+			result.add(i ^ (i >> 1));
+		return result;
+	}
+}
