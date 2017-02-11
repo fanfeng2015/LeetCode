c0af403d53803a818acb5bc331d5695ba5dbb40d
Fan <fanfeng2015@gmail.com>
2017-02-10 22:59:53 -0500

    #287

--- CanIWin/src/canIWin/CanIWin.java
+++ CanIWin/src/canIWin/CanIWin.java
@@ -5,7 +5,7 @@ public class CanIWin {
 	public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
 		return true;
 	}
-	
+
 	public static void main(String[] args) {
 		char[] array = new char[] { ' ', ' ' };
 		String s = new String(array);
