03fc892ef0b41f52c268c79812cc3ad4b52a3e6c
Fan <fanfeng2015@gmail.com>
2017-02-12 22:25:49 -0500

    #172

---
+++ FactorialTrailingZeros/src/factorialTrailingZeros/FactorialTrailingZeros.java
@@ -0,0 +1,27 @@
+package factorialTrailingZeros;
+
+// LeetCode #172
+
+// Given an integer n, return the number of trailing zeroes in n!.
+
+public class FactorialTrailingZeros {
+
+	public int trailingZeroes(int n) {
+		return n < 5 ? 0 : n / 5 + trailingZeroes(n / 5);
+	}
+
+	// Time complexity is O(log(n)).
+	// Space complexity is O(log(n)).
+
+	public int trailingZeroes2(int n) {
+		int count = 0;
+		while (n > 0) {
+			count += n / 5;
+			n /= 5;
+		}
+		return count;
+	}
+
+	// Time complexity is O(log(n)).
+	// Space complexity is O(1).
+}
