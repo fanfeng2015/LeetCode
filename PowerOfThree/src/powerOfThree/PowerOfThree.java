620da28f799718fff50f5847f3ac784888431823
Fan <fanfeng2015@gmail.com>
2017-02-18 15:00:49 -0500

    #326

---
+++ PowerOfThree/src/powerOfThree/PowerOfThree.java
@@ -0,0 +1,15 @@
+package powerOfThree;
+
+// LeetCode #326
+
+// Given an integer, write a function to determine if it is a power of three.
+
+// Could you do it without using any loop / recursion?
+
+public class PowerOfThree {
+
+	public boolean isPowerOfThree(int n) {
+		return (n > 0 && 1162261467 % n == 0);
+	}
+
+}
