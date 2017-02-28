f4a76aa9dcca34dec787f7f61167d60d2d4ebc02
Fan <fanfeng2015@gmail.com>
2017-02-27 22:28:55 -0500

    #311

--- SparseMatrixMultiplication/src/sparseMatrixMultiplication/SparseMatrixMultiplication.java
+++ SparseMatrixMultiplication/src/sparseMatrixMultiplication/SparseMatrixMultiplication.java
@@ -1,5 +1,8 @@
 package sparseMatrixMultiplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // LeetCode #311
 // Given two sparse matrices A and B, return the result of AB.
 // You may assume that A's column number is equal to B's row number.
@@ -23,4 +26,42 @@ public class SparseMatrixMultiplication {
 		}
 		return result;
 	}
+
+	static class Cell {
+		int col;
+		int val;
+
+		Cell(int col, int val) {
+			this.col = col;
+			this.val = val;
+		}
+	}
+
+	public int[][] multiply2(int[][] A, int[][] B) {
+		int rowA = A.length, colA = A[0].length;
+		int rowB = B.length, colB = B[0].length;
+		int[][] result = new int[rowA][colB];
+		List[] lists = new List[rowA];
+		for (int i = 0; i < rowA; i++) {
+			List<Cell> cur = new ArrayList<>();
+			for (int j = 0; j < colA; j++) {
+				if (A[i][j] != 0) {
+					cur.add(new Cell(j, A[i][j]));
+				}
+			}
+			lists[i] = cur;
+		}
+		for (int i = 0; i < rowA; i++) {
+			List<Cell> cur = lists[i];
+			for (int j = 0; j < cur.size(); j++) {
+				int col = cur.get(j).col;
+				int val = cur.get(j).val;
+				for (int k = 0; k < colB; k++) {
+					result[i][k] += val * B[col][k];
+				}
+			}
+		}
+		return result;
+	}
+
 }
