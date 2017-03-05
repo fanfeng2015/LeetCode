5cf4adf2d122633235494a971edb51e06af7f586
Fan <fanfeng2015@gmail.com>
2017-03-05 14:24:40 -0500

    #31

---
+++ NextPermutation/src/nextPermutation/NextPermutation.java
@@ -0,0 +1,51 @@
+package nextPermutation;
+
+// LeetCode #31
+
+// Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+// If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+// The replacement must be in-place, do not allocate extra memory.
+// Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+public class NextPermutation {
+
+	public void nextPermutation(int[] nums) {
+		int k = -1;
+		for (int i = nums.length - 2; i >= 0; i--) {
+			if (nums[i] < nums[i + 1]) {
+				k = i;
+				break;
+			}
+		}
+		if (k == -1) {
+			reverse(nums, 0, nums.length - 1);
+			return;
+		}
+		for (int i = nums.length - 1; i > k; i--) {
+			if (nums[i] > nums[k]) {
+				swap(nums, k, i);
+				reverse(nums, k + 1, nums.length - 1);
+				return;
+			}
+		}
+	}
+
+	private void reverse(int[] nums, int left, int right) {
+		while (left < right) {
+			int temp = nums[left];
+			nums[left] = nums[right];
+			nums[right] = temp;
+			left++;
+			right--;
+		}
+	}
+
+	private void swap(int[] nums, int left, int right) {
+		int temp = nums[left];
+		nums[left] = nums[right];
+		nums[right] = temp;
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(1).
+}
