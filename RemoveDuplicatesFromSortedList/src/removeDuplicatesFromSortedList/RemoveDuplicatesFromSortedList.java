cd0af1404be907314ce122d129388023e49d1ee2
Fan <fanfeng2015@gmail.com>
2017-03-06 14:19:28 -0500

    #83

--- RemoveDuplicatesFromSortedList/src/removeDuplicatesFromSortedList/RemoveDuplicatesFromSortedList.java
+++ RemoveDuplicatesFromSortedList/src/removeDuplicatesFromSortedList/RemoveDuplicatesFromSortedList.java
@@ -6,6 +6,7 @@ package removeDuplicatesFromSortedList;
 
 public class RemoveDuplicatesFromSortedList {
 
+	// Iterative Solution
 	public ListNode deleteDuplicates(ListNode head) {
 		if (head == null) {
 			return head;
@@ -25,4 +26,18 @@ public class RemoveDuplicatesFromSortedList {
 
 	// Time complexity is O(n).
 	// Space complexity is O(1).
+
+	// Recursive Solution
+	public ListNode deleteDuplicates2(ListNode head) {
+		// base case
+		if (head == null || head.next == null) {
+			return head;
+		}
+		// recursive rule
+		head.next = deleteDuplicates(head.next);
+		return head.val == head.next.val ? head.next : head;
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(n).
 }
