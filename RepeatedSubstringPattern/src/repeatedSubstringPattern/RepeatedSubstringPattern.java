87e48d8137e1d218732a3266438dfdb7d4200f74
Fan <fanfeng2015@gmail.com>
2017-03-12 00:33:22 -0500

    #459

--- RepeatedSubstringPattern/src/repeatedSubstringPattern/RepeatedSubstringPattern.java
+++ RepeatedSubstringPattern/src/repeatedSubstringPattern/RepeatedSubstringPattern.java
@@ -1,6 +1,7 @@
 package repeatedSubstringPattern;
 
 // LeetCode #459
+
 // Given a non-empty string check if it can be constructed by taking a substring of it 
 // and appending multiple copies of the substring together. 
 // You may assume the given string consists of lower-case English letters only, and its length will not exceed 10000.
