319a3718130d705e6065d75bb60590de2e98c269
Fan <fanfeng2015@gmail.com>
2017-06-11 15:08:46 -0700

    #454

--- FourSum2/src/fourSum2/FourSum2.java
+++ FourSum2/src/fourSum2/FourSum2.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 // LeetCode #454
 
+// Given four lists A, B, C, D of integer values, compute how many tuples 
+// (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+
+// To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. 
+// All integers are in the range of -2^28 to 2^28 - 1 and the result is guaranteed to be
+// at most 2^31 - 1.
+
 public class FourSum2 {
 
 	public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
