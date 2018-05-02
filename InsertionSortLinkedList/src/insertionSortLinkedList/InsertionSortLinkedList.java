7ae7cca755f8e253cebb752bbcbd54acc3c7d77c
Fan Feng <fanfeng2015@gmail.com>
2018-05-02 11:24:34 -0700

    #147

--- InsertionSortLinkedList/src/insertionSortLinkedList/InsertionSortLinkedList.java
+++ InsertionSortLinkedList/src/insertionSortLinkedList/InsertionSortLinkedList.java
@@ -1,27 +1,26 @@
 package insertionSortLinkedList;
 
-// LeetCode #147
+// LeetCode #147 (Insertion Sort List).
 
 // Sort a linked list using insertion sort.
 
 public class InsertionSortLinkedList {
 
-	public ListNode insertionSortList(ListNode head) {
-		ListNode dummy = new ListNode(0);
-		ListNode cur = head, next = null;
-		while (cur != null) {
-			next = cur.next;
-			ListNode prev = dummy;
-			while (prev.next != null && prev.next.val < cur.val) {
-				prev = prev.next;
-			}
-			// insert cur between prev and prev.next
-			cur.next = prev.next;
-			prev.next = cur;
-			cur = next;
-		}
-		return dummy.next;
-	}
+    public ListNode insertionSortList(ListNode head) {
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            ListNode min = cur;
+            for (ListNode iter = min.next; iter != null; iter = iter.next) {
+                if (iter.val < min.val) {
+                    min = iter;
+                }
+            }
+            // swap
+            int temp = cur.val;
+            cur.val = min.val;
+            min.val = temp;
+        }
+        return head;
+    }
 
 	// Time complexity is O(n^2).
 	// Space complexity is O(1).
