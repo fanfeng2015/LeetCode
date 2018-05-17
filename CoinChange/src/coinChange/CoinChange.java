4570b5f927804cf54baad06c0c559a989eb35da2
Fan Feng <fanfeng2015@gmail.com>
2018-05-16 21:51:32 -0700

    LeetCode #322

--- CoinChange/src/coinChange/CoinChange.java
+++ CoinChange/src/coinChange/CoinChange.java
@@ -2,7 +2,7 @@ package coinChange;
 
 import java.util.Arrays;
 
-// LeetCode #322
+// LeetCode #322 (Coin Change).
 
 // You are given coins of different denominations and a total amount of money amount. 
 // Write a function to compute the fewest number of coins that you need to make up that amount. 
@@ -13,16 +13,16 @@ public class CoinChange {
 	// M[i]: fewest number of coins that are needed to make up amount i
 	public int coinChange(int[] coins, int amount) {
 		int[] M = new int[amount + 1];
-		Arrays.fill(M, Integer.MAX_VALUE);
+		Arrays.fill(M, amount + 1);
 		M[0] = 0;
 		for (int i = 1; i <= amount; i++) {
 			for (int j = 0; j < coins.length; j++) {
-				if (i - coins[j] >= 0 && M[i - coins[j]] != Integer.MAX_VALUE) {
+				if (i - coins[j] >= 0) {
 					M[i] = Math.min(M[i], M[i - coins[j]] + 1);
 				}
 			}
 		}
-		return M[amount] == Integer.MAX_VALUE ? -1 : M[amount];
+		return M[amount] == amount + 1 ? -1 : M[amount];
 	}
 
 	// Time complexity is O(n*amount).
