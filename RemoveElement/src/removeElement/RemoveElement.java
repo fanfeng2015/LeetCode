1551dd8d2c40bfba00ce5f4e7f630f8dbe2a20e1
Fan Feng <fanfeng2015@gmail.com>
2018-05-21 21:20:18 -0700

    LeetCode #27

--- RemoveElement/src/removeElement/RemoveElement.java
+++ RemoveElement/src/removeElement/RemoveElement.java
@@ -1,6 +1,6 @@
 package removeElement;
 
-// LeetCode #27
+// LeetCode #27 (Remove Element).
 
 // Given an array and a value, remove all instances of that value in place and return the new length.
 // Do not allocate extra space for another array, you must do this in place with constant memory.
