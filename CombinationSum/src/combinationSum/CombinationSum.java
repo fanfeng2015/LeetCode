b87acf145fdff2683771d0984892c3a760892fa9
Fan Feng <fanfeng2015@gmail.com>
2018-05-26 13:14:11 -0700

    LeetCode #39

--- CombinationSum/src/combinationSum/CombinationSum.java
+++ CombinationSum/src/combinationSum/CombinationSum.java
@@ -14,11 +14,11 @@ public class CombinationSum {
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		List<Integer> cur = new ArrayList<>();
-		DFS(result, cur, candidates, target, 0);
+		DFS(candidates, target, cur, result, 0);
 		return result;
 	}
 
-	private void DFS(List<List<Integer>> result, List<Integer> cur, int[] candidates, int target, int start) {
+	private void DFS(int[] candidates, int target, List<Integer> cur, List<List<Integer>> result, int start) {
 		if (target < 0) {
 			return;
 		} else if (target == 0) {
@@ -27,7 +27,7 @@ public class CombinationSum {
 		}
 		for (int i = start; i < candidates.length; i++) {
 			cur.add(candidates[i]);
-			DFS(result, cur, candidates, target - candidates[i], i);
+			DFS(candidates, target - candidates[i], cur, result, i);
 			cur.remove(cur.size() - 1);
 		}
 	}
@@ -39,25 +39,25 @@ public class CombinationSum {
 	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		List<Integer> cur = new ArrayList<>();
-		DFS2(result, cur, candidates, target, 0);
+		DFS2(candidates, target, cur, result, 0);
 		return result;
 	}
 
-	private void DFS2(List<List<Integer>> result, List<Integer> cur, int[] candidates, int target, int level) {
+	private void DFS2(int[] candidates, int target, List<Integer> cur, List<List<Integer>> result, int level) {
 		if (level == candidates.length) {
 			if (target == 0) {
-				add(result, cur, candidates);
+				add(candidates, cur, result);
 			}
 			return;
 		}
 		for (int i = 0; i <= target / candidates[level]; i++) {
 			cur.add(i);
-			DFS2(result, cur, candidates, target - i * candidates[level], level + 1);
+			DFS2(candidates, target - i * candidates[level], cur, result, level + 1);
 			cur.remove(cur.size() - 1);
 		}
 	}
 
-	private void add(List<List<Integer>> result, List<Integer> cur, int[] candidates) {
+	private void add(int[] candidates, List<Integer> cur, List<List<Integer>> result) {
 		List<Integer> r = new ArrayList<>();
 		for (int i = 0; i < cur.size(); i++) { // add candidates[i] cur.get(i) many times
 			for (int j = 0; j < cur.get(i); j++) {
