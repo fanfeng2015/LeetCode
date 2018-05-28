09dc91cee048e93c06470995790db366e8e033d0
Fan Feng <fanfeng2015@gmail.com>
2018-05-28 14:07:28 -0700

    LeetCode #487

--- MaximumConsecutiveOnes2/src/maximumConsecutiveOnes2/MaximumConsecutiveOnes2.java
+++ MaximumConsecutiveOnes2/src/maximumConsecutiveOnes2/MaximumConsecutiveOnes2.java
@@ -1,5 +1,9 @@
 package maximumConsecutiveOnes2;
 
+import java.util.LinkedList;
+
+// LeetCode #487 (Maximum Consecutive Ones II).
+
 // Given a binary array, find the maximum number of consecutive 1s in this array 
 // if you can flip at most one 0.
 
@@ -13,10 +17,10 @@ public class MaximumConsecutiveOnes2 {
         int zeros = 0, max = 0;
         for (int left = 0, right = 0; right < nums.length; right++) {
             zeros = nums[right] == 0 ? zeros + 1 : zeros;
-            while (zeros > 1) {
+            while (zeros > 1) { // can be generalized to k
                 zeros = nums[left++] == 0 ? zeros - 1 : zeros;
             }
-            // guaranteed that range [left, right] contains exactly one 0
+            // guaranteed that range [left, right] contains exactly one (or k) 0
             max = Math.max(max, right - left + 1);
         }
         return max;
@@ -24,4 +28,24 @@ public class MaximumConsecutiveOnes2 {
     
     // Time complexity is O(n).
     // Space complexity is O(1).
+
+    // Follow up: What if the input numbers come in one by one as an infinite stream?
+    // In other words, you can't store all numbers coming from the stream as it's too
+    // large to hold in memory. 
+
+    public int findMaxConsecutiveOnes2(int[] nums) {
+        int max = 0;
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int left = 0, right = 0; right < nums.length; right++) {
+        	if (nums[right] == 0) {
+        		queue.offerFirst(right);
+        	}
+        	if (queue.size() > 1) { // can be generalized to k
+        		left = queue.pollLast() + 1;
+        	}
+            // guaranteed that range [left, right] contains exactly one (or k) 0
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
 }
