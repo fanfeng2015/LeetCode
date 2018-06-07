d99614e3970d053fbe3e0dd7310b5182408279f2
Fan Feng <fanfeng2015@gmail.com>
2018-06-07 13:56:16 -0700

    LeetCode #113

--- PathSum3/src/pathSum3/PathSum3.java
+++ PathSum3/src/pathSum3/PathSum3.java
@@ -3,7 +3,7 @@ package pathSum3;
 import java.util.HashMap;
 import java.util.Map;
 
-// LeetCode #437
+// LeetCode #437 (Path Sum III).
 
 // You are given a binary tree in which each node contains an integer value.
 // Find the number of paths that sum to a given value.
@@ -14,57 +14,32 @@ import java.util.Map;
 
 public class PathSum3 {
 
-	// Solution 1
-	public int pathSum(TreeNode root, int sum) {
-		if (root == null) {
-			return 0;
-		}
-		return helper(root, sum) + helper(root.left, sum) + helper(root.right, sum);
-	}
-
-	private int helper(TreeNode root, int sum) {
-		int count = 0;
-		if (root == null) {
-			return count;
-		}
-		if (sum == root.val) {
-			count++;
-		}
-		count += helper(root.left, sum - root.val);
-		count += helper(root.right, sum - root.val);
-		return count;
-	}
-
-	// Time complexity is O(n^2).
-	// Space complexity is O(n), when the binary tree is highly unbalanced.
-
-	// Solution 2
 	private int count = 0;
 
-	public int pathSum2(TreeNode root, int sum) {
-		Map<Integer, Integer> prefixSums = new HashMap<>();
-		prefixSums.put(0, 1);
-		helper2(root, prefixSums, 0, sum);
+	public int pathSum(TreeNode root, int sum) {
+		Map<Integer, Integer> prefixSumMap = new HashMap<>();
+		prefixSumMap.put(0, 1);
+		pathSum(root, sum, 0, prefixSumMap);
 		return count;
 	}
 
-	private void helper2(TreeNode root, Map<Integer, Integer> prefixSums, int prevSum, int target) {
+	private void pathSum(TreeNode root, int target, int prefixSum, Map<Integer, Integer> prefixSumMap) {
 		if (root == null) {
 			return;
 		}
-		prevSum += root.val;
-		if (prefixSums.containsKey(prevSum - target)) {
-			count += prefixSums.get(prevSum - target);
+		prefixSum += root.val;
+		if (prefixSumMap.containsKey(prefixSum - target)) {
+			count += prefixSumMap.get(prefixSum - target);
 		}
-		// base case
-		Integer i = prefixSums.get(prevSum);
-		i = (i == null) ? 1 : i + 1;
-		prefixSums.put(prevSum, i);
-		// recursive rule
-		helper2(root.left, prefixSums, prevSum, target);
-		helper2(root.right, prefixSums, prevSum, target);
-		// decrement by 1
-		prefixSums.put(prevSum, prefixSums.get(prevSum) - 1);
+		// increment
+		Integer frequency = prefixSumMap.get(prefixSum);
+		frequency = (frequency == null) ? 1 : frequency + 1;
+		prefixSumMap.put(prefixSum, frequency);
+		// dfs
+		pathSum(root.left, target, prefixSum, prefixSumMap);
+		pathSum(root.right, target, prefixSum, prefixSumMap);
+		// decrement
+		prefixSumMap.put(prefixSum, prefixSumMap.get(prefixSum) - 1);
 	}
 
 	// Time complexity is O(n).
