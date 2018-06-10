9967d26ae19c00441c34b55c4c3c0c24c9b08c89
Fan Feng <fanfeng2015@gmail.com>
2018-06-10 16:24:45 -0700

    LeetCode #244

--- ShortestWordDistance2/src/shortestWordDistance2/ShortestWordDistance2.java
+++ ShortestWordDistance2/src/shortestWordDistance2/ShortestWordDistance2.java
@@ -5,13 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// LeetCode #244
+// LeetCode #244 (Shortest Word Distance II).
 
-// This is a follow up of Shortest Word Distance. 
-// The only difference is now you are given the list of words and your method will be called repeatedly many times with different parameters. 
-// How would you optimize it?
-// Design a class which receives a list of words in the constructor, and implements a method that takes two words word1 and word2 
-// and return the shortest distance between these two words in the list.
+// This is a follow up of Shortest Word Distance.
+
+// The only difference is now you are given the list of words and your method will be called 
+// repeatedly many times with different parameters. How would you optimize it?
+
+// Design a class which receives a list of words in the constructor, and implements a method
+// that takes two words word1 and word2 and return the shortest distance between these two words in the list.
 
 public class ShortestWordDistance2 {
 
@@ -28,17 +30,14 @@ public class ShortestWordDistance2 {
 	}
 
 	public int shortest(String word1, String word2) {
-		List<Integer> list1 = map.get(word1), list2 = map.get(word2);
 		int min = Integer.MAX_VALUE;
+		List<Integer> list1 = map.get(word1), list2 = map.get(word2);
 		int i = 0, j = 0;
 		while (i < list1.size() && j < list2.size()) {
-			int index1 = list1.get(i), index2 = list2.get(j);
-			if (index1 < index2) {
-				min = Math.min(min, index2 - index1);
-				i++;
-			} else if (index1 > index2) {
-				min = Math.min(min, index1 - index2);
-				j++;
+			if (list1.get(i) < list2.get(j)) {
+				min = Math.min(min, list2.get(j) - list1.get(i++));
+			} else if (list1.get(i) > list2.get(j)) {
+				min = Math.min(min, list1.get(i) - list2.get(j++));
 			}
 		}
 		return min;
