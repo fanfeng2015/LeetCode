74a6668d1d47c5b69782db341aa2335baa37c21d
Fan Feng <fanfeng2015@gmail.com>
2018-06-10 15:47:40 -0700

    LeetCode #243

--- ShortestWordDistance/src/shortestWordDistance/ShortestWordDistance.java
+++ ShortestWordDistance/src/shortestWordDistance/ShortestWordDistance.java
@@ -1,6 +1,6 @@
 package shortestWordDistance;
 
-// LeetCode #243
+// LeetCode #243 (Shortest Word Distance).
 
 // Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
 
@@ -10,12 +10,8 @@ public class ShortestWordDistance {
 		int one = -1, two = -1;
 		int min = Integer.MAX_VALUE;
 		for (int i = 0; i < words.length; i++) {
-			if (words[i].equals(word1)) {
-				one = i;
-			}
-			if (words[i].equals(word2)) {
-				two = i;
-			}
+			one = words[i].equals(word1) ? i : one;
+			two = words[i].equals(word2) ? i : two;
 			if (one != -1 && two != -1) {
 				min = Math.min(min, Math.abs(one - two));
 			}
