a02ad88e1c8f05b1564a06791861b5068f052e39
Fan Feng <fanfeng2015@gmail.com>
2018-06-10 17:55:00 -0700

    LeetCode #79

--- WordSearch/src/wordSearch/WordSearch.java
+++ WordSearch/src/wordSearch/WordSearch.java
@@ -1,6 +1,6 @@
 package wordSearch;
 
-// LeetCode #79
+// LeetCode #79 (Word Search).
 
 // Given a 2D board and a word, find if the word exists in the grid.
 // The word can be constructed from letters of sequentially adjacent cell, 
@@ -12,14 +12,13 @@ public class WordSearch {
 	private static int[][] directions = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
 	public boolean exist(char[][] board, String word) {
-		int m = board.length;
-		int n = board[0].length;
+		int m = board.length, n = board[0].length;
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
 				if (board[i][j] == word.charAt(0)) {
 					boolean[][] visited = new boolean[m][n];
 					visited[i][j] = true;
-					if (DFS(board, word, i, j, 1, visited)) {
+					if (DFS(board, word, 1, i, j, visited)) {
 						return true;
 					}
 				}
@@ -28,26 +27,27 @@ public class WordSearch {
 		return false;
 	}
 
-	private boolean DFS(char[][] board, String word, int row, int col, int pos, boolean[][] visited) {
-		// base case
-		if (pos == word.length()) {
+	private boolean DFS(char[][] board, String word, int start, int row, int col, boolean[][] visited) {
+		if (start == word.length()) {
 			return true;
 		}
 		for (int[] direction : directions) {
-			if (0 <= row + direction[0] && row + direction[0] <= board.length - 1 && 0 <= col + direction[1]
-					&& col + direction[1] <= board[0].length - 1
-					&& board[row + direction[0]][col + direction[1]] == word.charAt(pos)
-					&& !visited[row + direction[0]][col + direction[1]]) {
-				visited[row + direction[0]][col + direction[1]] = true;
-				if (DFS(board, word, row + direction[0], col + direction[1], pos + 1, visited)) {
+			int newRow = row + direction[0], newCol = col + direction[1];
+			if (newRow >= 0 && newRow < board.length 
+				&& newCol >= 0 && newCol < board[0].length
+				&& board[newRow][newCol] == word.charAt(start)
+				&& !visited[row + direction[0]][col + direction[1]]) 
+			{
+				visited[newRow][newCol] = true;
+				if (DFS(board, word, start + 1, newRow, newCol, visited)) {
 					return true;
 				}
-				visited[row + direction[0]][col + direction[1]] = false;
+				visited[newRow][newCol] = false;
 			}
 		}
 		return false;
 	}
 
-	// Time complexity is O(m*n * 4^(m*n))?
+	// Time complexity is O(m*n * 4^(m*n)).
 	// Space complexity is O(m*n)?
 }
