9115d6a1765afef5797435998860947ee318bf31
Fan Feng <fanfeng2015@gmail.com>
2018-06-10 18:18:24 -0700

    LeetCode #212

---
+++ WordSearch2/src/wordSearch2/WordSearch2.java
@@ -0,0 +1,13 @@
+package wordSearch2;
+
+// LeetCode #212 (Word Search II).
+
+// Given a 2D board and a list of words from the dictionary, find all words in the board.
+// Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" 
+// cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+
+public class WordSearch2 {
+
+	// Obviously, LeetCode #79 (Word Search) solves the problem.
+	
+}
