d174986bf57dcea67a8a7f4000db545f03300384
Fan Feng <fanfeng2015@gmail.com>
2018-06-14 17:42:00 -0700

    LeetCode #200

--- NumberOfIslands/src/numberOfIslands/NumberOfIslands.java
+++ NumberOfIslands/src/numberOfIslands/NumberOfIslands.java
@@ -1,9 +1,12 @@
 package numberOfIslands;
 
-// LeetCode #200
+// LeetCode #200 (Number of Islands).
 
-// Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
-// An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+// Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+
+// An island is surrounded by water and is formed by connecting adjacent lands horizontally
+// or vertically. 
+
 // You may assume all four edges of the grid are all surrounded by water.
 
 public class NumberOfIslands {
@@ -27,7 +30,6 @@ public class NumberOfIslands {
 	}
 
 	private void DFS(char[][] grid, int row, int col) {
-		// base case
 		if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] != '1') {
 			return;
 		}
@@ -40,5 +42,4 @@ public class NumberOfIslands {
 
 	// Time complexity is O(m*n).
 	// Space complexity is O(m+n).
-
 }
