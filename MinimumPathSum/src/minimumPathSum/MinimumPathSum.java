c1f4bee6978c29530b6576bebfef5ad5e2ac902e
Fan Feng <fanfeng2015@gmail.com>
2018-06-19 12:22:20 -0700

    LeetCode #64

--- MinimumPathSum/src/minimumPathSum/MinimumPathSum.java
+++ MinimumPathSum/src/minimumPathSum/MinimumPathSum.java
@@ -1,9 +1,10 @@
 package minimumPathSum;
 
-// LeetCode #64
+// LeetCode #64 (Minimum Path Sum).
 
-// Given a m x n grid filled with non-negative numbers, 
-// find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+// Given a m x n grid filled with non-negative numbers, find a path from top left 
+// to bottom right which minimizes the sum of all numbers along its path.
+
 // Note: You can only move either down or right at any point in time.
 
 public class MinimumPathSum {
@@ -13,16 +14,14 @@ public class MinimumPathSum {
 		int[][] M = new int[m][n];
 		for (int i = m - 1; i >= 0; i--) {
 			for (int j = n - 1; j >= 0; j--) {
-				if (i == m - 1) {
-					M[i][j] = grid[i][j] + (j == n - 1 ? 0 : M[i][j + 1]);
-				} else {
-					M[i][j] = grid[i][j] + (j == n - 1 ? M[i + 1][j] : Math.min(M[i + 1][j], M[i][j + 1]));
-				}
+				int down = i < m - 1 ? M[i + 1][j] : Integer.MAX_VALUE;
+				int right = j < n - 1 ? M[i][j + 1] : Integer.MAX_VALUE;
+				M[i][j] = grid[i][j] + ((i == m - 1 && j == n - 1) ? 0 : Math.min(down, right));
 			}
 		}
 		return M[0][0];
 	}
 
 	// Time complexity is O(m*n).
-	// Space complexity is O(m*n), but we really only need O(n).
+	// Space complexity is O(m*n), but obviously can be optimized to O(n).
 }
