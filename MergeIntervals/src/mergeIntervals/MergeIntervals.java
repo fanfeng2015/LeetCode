0ca006c4730a030b4b39848bf137b4f68d76c8e4
Fan Feng <fanfeng2015@gmail.com>
2018-06-20 11:41:43 -0700

    LeetCode #56

--- MergeIntervals/src/mergeIntervals/MergeIntervals.java
+++ MergeIntervals/src/mergeIntervals/MergeIntervals.java
@@ -5,12 +5,27 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-// LeetCode #56
+// LeetCode #56 (Merge Intervals).
+
 // Given a collection of intervals, merge all overlapping intervals.
 
 public class MergeIntervals {
 
-	public class MyComparator implements Comparator<Interval> {
+	public List<Interval> merge(List<Interval> intervals) {
+		List<Interval> result = new ArrayList<Interval>();
+		Collections.sort(intervals, new IntervalComparator());
+		for (Interval interval : intervals) {
+			Interval lastInterval = result.size() > 0 ? result.get(result.size() - 1) : null;
+			if (lastInterval == null || lastInterval.end < interval.start) {
+				result.add(interval);
+			} else {
+				lastInterval.end = Math.max(lastInterval.end, interval.end);
+			}
+		}
+		return result;
+	}
+
+	private class IntervalComparator implements Comparator<Interval> {
 		@Override
 		public int compare(Interval i1, Interval i2) {
 			if (i1.start == i2.start) {
@@ -20,20 +35,6 @@ public class MergeIntervals {
 		}
 	}
 
-	public List<Interval> merge(List<Interval> intervals) {
-		List<Interval> result = new ArrayList<Interval>();
-		Collections.sort(intervals, new MyComparator());
-		for (int i = 0; i < intervals.size(); i++) {
-			if (result.size() == 0 || intervals.get(i).start > result.get(result.size() - 1).end) {
-				result.add(intervals.get(i));
-			} else {
-				result.get(result.size() - 1).end = Math.max(result.get(result.size() - 1).end, intervals.get(i).end);
-			}
-		}
-		return result;
-	}
-
-	// Time complexity is O(n * log(n)).
-	// Space complexity is O(n), because Java sorts non-primitive types with
-	// Merge Sort.
+	// Time complexity is O(n*log(n)).
+	// Space complexity is O(n), because of merge sort (for non-primitive types).
 }
