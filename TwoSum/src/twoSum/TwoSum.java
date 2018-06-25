48bd2e4694b7f8e1a50fe5bb8a052df38fe3750c
Fan Feng <fanfeng2015@gmail.com>
2018-06-25 13:52:42 -0700

    LeetCode #1

--- TwoSum/src/twoSum/TwoSum.java
+++ TwoSum/src/twoSum/TwoSum.java
@@ -3,14 +3,16 @@ package twoSum;
 import java.util.HashMap;
 import java.util.Map;
 
-// LeetCode #1
+// LeetCode #1 (Two Sum).
 
-// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// Given an array of integers, return indices of the two numbers such that they 
+// add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may 
+// not use the same element twice.
 
 public class TwoSum {
 
-	// To return indices, array cannot be sorted.
 	public int[] twoSum(int[] nums, int target) {
 		Map<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < nums.length; i++) {
