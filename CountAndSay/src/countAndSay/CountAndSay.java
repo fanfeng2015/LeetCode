89007269fcd3f73cbca079655a0b0921b32533a1
Fan Feng <fanfeng2015@gmail.com>
2018-06-24 17:17:51 -0700

    LeetCode #38

--- CountAndSay/src/countAndSay/CountAndSay.java
+++ CountAndSay/src/countAndSay/CountAndSay.java
@@ -1,6 +1,6 @@
 package countAndSay;
 
-// LeetCode #38
+// LeetCode #38 (Count and Say).
 
 // The count-and-say sequence is the sequence of integers beginning as follows:
 // 1, 11, 21, 1211, 111221, ...
@@ -11,18 +11,18 @@ public class CountAndSay {
 		StringBuilder sb = new StringBuilder("1");
 		for (int i = 1; i < n; i++) {
 			String s = sb.toString();
-			sb = new StringBuilder();
-			int index = 0, count = 1;
-			while (index < s.length()) {
-				while (index < s.length() - 1 && s.charAt(index) == s.charAt(index + 1)) {
-					count++;
-					index++;
+			sb.delete(0, sb.length());
+			int start = 0, cur = 1;
+			while (cur < s.length()) {
+				while (cur < s.length() - 1 && s.charAt(cur) == s.charAt(start)) {
+					cur++;
 				}
-				sb.append(count).append(s.charAt(index));
-				index++;
-				count = 1;
+				sb.append(cur - start).append(s.charAt(start));
+				start = cur;
 			}
 		}
 		return sb.toString();
 	}
+
+	// Complexity depends on f(n), i.e., the length of the n-th sequence.
 }
