0a23d6737e5cb802f6db0fbc5e83e5c582f106cd
Fan Feng <fanfeng2015@gmail.com>
2018-06-29 21:35:53 -0700

    LeetCode #198

--- HouseRobber/src/houseRobber/HouseRobber.java
+++ HouseRobber/src/houseRobber/HouseRobber.java
@@ -1,6 +1,6 @@
 package houseRobber;
 
-// LeetCode #198
+// LeetCode #198 (House Robber).
 
 // You are a professional robber planning to rob houses along a street. 
 // Each house has a certain amount of money stashed, the only constraint 
@@ -12,32 +12,11 @@ package houseRobber;
 
 public class HouseRobber {
 
-	// Solution 1
 	public int rob(int[] nums) {
 		if (nums == null || nums.length == 0) {
 			return 0;
 		}
-		int[] M = new int[nums.length];
-		int result = 0;
-		for (int i = 0; i < nums.length; i++) {
-			M[i] = nums[i];
-			for (int j = 0; j < i - 1; j++) {
-				M[i] = Math.max(M[i], M[j] + nums[i]);
-			}
-			result = Math.max(result, M[i]);
-		}
-		return result;
-	}
-
-	// Time complexity is O(n^2).
-	// Space complexity is O(n).
-
-	// Solution 2: Optimization
-	public int rob2(int[] nums) {
-		if (nums == null || nums.length == 0) {
-			return 0;
-		}
-		int odd = 0, even = 0;
+		int even = 0, odd = 0;
 		for (int i = 0; i < nums.length; i++) {
 			if (i % 2 == 0) {
 				even = Math.max(even + nums[i], odd);
@@ -45,7 +24,7 @@ public class HouseRobber {
 				odd = Math.max(odd + nums[i], even);
 			}
 		}
-		return Math.max(odd, even);
+		return Math.max(even, odd);
 	}
 
 	// Time complexity is O(n).
