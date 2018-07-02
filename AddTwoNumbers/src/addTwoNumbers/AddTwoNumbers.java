0a6f70148284437a1c7d05ebd31c73786af1044f
Fan Feng <fanfeng2015@gmail.com>
2018-07-02 16:32:06 -0700

    LeetCode #2

--- AddTwoNumbers/src/addTwoNumbers/AddTwoNumbers.java
+++ AddTwoNumbers/src/addTwoNumbers/AddTwoNumbers.java
@@ -1,16 +1,19 @@
 package addTwoNumbers;
 
-// LeetCode #2
-// You are given two non-empty linked lists representing two non-negative integers. 
+// LeetCode #2 (Add Two Numbers).
+
+// You are given two non-empty linked lists representing two non-negative integers.
+
 // The digits are stored in reverse order and each of their nodes contains a single digit. 
+
 // Add the two numbers and return it as a linked list.
+
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 public class AddTwoNumbers {
 
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode head = new ListNode(0);
-		ListNode cur = head;
+		ListNode head = new ListNode(0), cur = head;
 		int carry = 0;
 		while (l1 != null || l2 != null || carry != 0) {
 			int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
@@ -25,5 +28,4 @@ public class AddTwoNumbers {
 
 	// Time complexity is O(m+n).
 	// Space complexity is O(1).
-
 }
