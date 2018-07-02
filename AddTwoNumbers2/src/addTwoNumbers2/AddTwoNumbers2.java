b97d0bec84662f2236266482921a36d05f05e836
Fan Feng <fanfeng2015@gmail.com>
2018-07-02 16:42:43 -0700

    LeetCode #445

--- AddTwoNumbers2/src/addTwoNumbers2/AddTwoNumbers2.java
+++ AddTwoNumbers2/src/addTwoNumbers2/AddTwoNumbers2.java
@@ -2,18 +2,22 @@ package addTwoNumbers2;
 
 import java.util.LinkedList;
 
-// LeetCode #445
+// LeetCode #445 (Add Two Numbers II).
 
 // You are given two non-empty linked lists representing two non-negative integers. 
+
 // The most significant digit comes first and each of their nodes contain a single digit. 
+
 // Add the two numbers and return it as a linked list.
+
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 // Example: 
 // Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
 // Output: 7 -> 8 -> 0 -> 7
 
-// Follow-up: What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+// Follow-up: What if you cannot modify the input lists? In other words, reversing the lists is
+// not allowed.
 
 public class AddTwoNumbers2 {
 
@@ -23,16 +27,15 @@ public class AddTwoNumbers2 {
 	}
 
 	private ListNode add(ListNode l1, ListNode l2) {
-		ListNode head = new ListNode(0);
-		ListNode cur = head;
+		ListNode head = new ListNode(0), cur = head;
 		int carry = 0;
 		while (l1 != null || l2 != null || carry != 0) {
 			int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
-			cur.next = new ListNode(sum % 10);
 			carry = sum / 10;
+			cur.next = new ListNode(sum % 10);
+			cur = cur.next;
 			l1 = (l1 == null) ? l1 : l1.next;
 			l2 = (l2 == null) ? l2 : l2.next;
-			cur = cur.next;
 		}
 		return head.next;
 	}
@@ -52,7 +55,7 @@ public class AddTwoNumbers2 {
 	// Time complexity is O(m + n).
 	// Space complexity is O(1), because reverse() is iterative.
 
-	// Solution 2: follow-up: use stack
+	// Solution 2: Follow-up: Use stack
 	public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
 		LinkedList<Integer> stack1 = new LinkedList<>(), stack2 = new LinkedList<>();
 		ListNode head1 = l1, head2 = l2;
