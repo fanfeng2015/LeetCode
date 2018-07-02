bd2bd8bd6f61d665fdff42d939090f2ce8cad5f4
Fan Feng <fanfeng2015@gmail.com>
2018-07-02 10:22:27 -0700

    LeetCode #337

--- HouseRobber3/src/houseRobber3/HouseRobber3.java
+++ HouseRobber3/src/houseRobber3/HouseRobber3.java
@@ -1,30 +1,30 @@
 package houseRobber3;
 
-// LeetCode #337
+// LeetCode #337 (House Robber III).
 
-// The thief has found himself a new place for his thievery again. 
-// There is only one entrance to this area, called the "root." 
-// Besides the root, each house has one and only one parent house. 
-// After a tour, the smart thief realized that "all houses in this place forms a binary tree". 
-// It will automatically contact the police if two directly-linked houses were broken into on the same night.
+// The thief has found himself a new place for his thievery again. There is only one entrance
+// to this area, called the "root." 
+
+// Besides the root, each house has one and only one parent house. After a tour, the smart thief
+// realized that "all houses in this place forms a binary tree". It will automatically contact 
+// the police if two directly-linked houses were broken into on the same night.
 
 // Determine the maximum amount of money the thief can rob tonight without alerting the police.
 
 public class HouseRobber3 {
 
 	public int rob(TreeNode root) {
-		int[] result = helper(root); // [rob, not rob]
+		int[] result = helper(root);
 		return Math.max(result[0], result[1]);
 	}
 
+	// [rob, not rob]
 	private int[] helper(TreeNode root) {
-		// base case
 		if (root == null) {
 			return new int[] { 0, 0 };
 		} else if (root.left == null && root.right == null) {
 			return new int[] { root.val, 0 };
 		}
-		// root.left != null || root.right != null
 		int[] left = helper(root.left);
 		int[] right = helper(root.right);
 		return new int[] { root.val + left[1] + right[1], Math.max(left[0], left[1]) + Math.max(right[0], right[1]) };
