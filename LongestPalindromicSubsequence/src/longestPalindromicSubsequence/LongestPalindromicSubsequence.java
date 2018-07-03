24139452f2de3ec9ed3ff34f48c24e73bc0542d4
Fan Feng <fanfeng2015@gmail.com>
2018-07-03 14:55:20 -0700

    LeetCode #516

--- LongestPalindromicSubsequence/src/longestPalindromicSubsequence/LongestPalindromicSubsequence.java
+++ LongestPalindromicSubsequence/src/longestPalindromicSubsequence/LongestPalindromicSubsequence.java
@@ -1,24 +1,20 @@
 package longestPalindromicSubsequence;
 
-// LeetCode #516
+// LeetCode #516 (Longest Palindromic Subsequence).
 
 // Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
 
 public class LongestPalindromicSubsequence {
 
-	// M[i][j]: length of the longest palindromic subsequence in substring from
-	// i to j, inclusive
+	// M[i][j]: length of the longest palindromic subsequence of s in [i, j]
 	public int longestPalindromeSubseq(String s) {
-		int n = s.length();
-		int[][] M = new int[n][n];
 		int max = 0;
-		for (int i = n - 1; i >= 0; i--) {
-			for (int j = i; j <= n - 1; j++) {
+		int[][] M = new int[s.length()][s.length()];
+		for (int i = s.length() - 1; i >= 0; i--) {
+			for (int j = i; j < s.length(); j++) {
 				if (i == j) {
 					M[i][j] = 1;
 				} else if (s.charAt(i) == s.charAt(j)) {
-					// fine even if i + 1 > j - 1, because cells in the
-					// bottom-left triangle are all zero
 					M[i][j] = M[i + 1][j - 1] + 2;
 				} else if (s.charAt(i) != s.charAt(j)) {
 					M[i][j] = Math.max(M[i + 1][j], M[i][j - 1]);
@@ -30,5 +26,5 @@ public class LongestPalindromicSubsequence {
 	}
 
 	// Time complexity is O(n^2).
-	// Space complexity is O(n^2), although we really only need O(n).
+	// Space complexity is O(n^2), but can obviously be optimized to O(n).
 }
