056cc2b70b473ccac764bffa9e07eac20449f42b
Fan Feng <fanfeng2015@gmail.com>
2018-07-09 12:34:10 -0700

    LeetCode #159

--- LongestSubstringWithAtMostTwoDistinctCharacters/src/longestSubstringWithAtMostTwoDistinctCharacters/LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ LongestSubstringWithAtMostTwoDistinctCharacters/src/longestSubstringWithAtMostTwoDistinctCharacters/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -2,45 +2,34 @@ package longestSubstringWithAtMostTwoDistinctCharacters;
 
 // LeetCode #159 (Longest Substring with At Most Two Distinct Characters).
 
-// Given a string s , find the length of the longest substring t  that contains 
+// Given a string s, find the length of the longest substring t that contains 
 // at most 2 distinct characters.
 
 public class LongestSubstringWithAtMostTwoDistinctCharacters {
 
 	public int lengthOfLongestSubstringTwoDistinct(String s) {
 		int max = 0, lo = 0;
-		Entity one = null, two = null;
+		int count = 0;
+		int[] countMap = new int[256];
 		for (int i = 0; i < s.length(); i++) {
-			if (one == null) {
-				one = new Entity(s.charAt(i), i);
-			} else if (s.charAt(i) == one.ch) {
-				one.lastOccurrence = i;
-			} else if (two == null) {
-				two = new Entity(s.charAt(i), i);
-			} else if (s.charAt(i) == two.ch) {
-				two.lastOccurrence = i;
-			} else if (one.lastOccurrence < two.lastOccurrence) {
-				lo = one.lastOccurrence + 1;
-				one = new Entity(s.charAt(i), i);
-			} else {
-				lo = two.lastOccurrence + 1;
-				two = new Entity(s.charAt(i), i);
+			if (countMap[s.charAt(i)]++ == 0) {
+				count++;
 			}
-			max = Math.max(max, i - lo + 1);
+			if (count <= 2) {
+				max = Math.max(max, i - lo + 1);
+			}
+			while (count > 2) {
+				if (countMap[s.charAt(lo++)]-- == 1) {
+					count--;
+				}
+			}
 		}
 		return max;
 	}
 
-	private static class Entity {
-		char ch;
-		int lastOccurrence;
-
-		Entity(char ch, int lastOccurrence) {
-			this.ch = ch;
-			this.lastOccurrence = lastOccurrence;
-		}
-	}
-
 	// Time complexity is O(n).
 	// Space complexity is O(1).
+
+	// Follow up: LeetCode #340 (Longest Substring with At Most K Distinct
+	// Characters).
 }
