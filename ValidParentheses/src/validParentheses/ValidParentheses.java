890dbcd89499c739e2b2a5a2293245c8bad6ff42
Fan Feng <fanfeng2015@gmail.com>
2018-07-09 12:55:46 -0700

    LeetCode #20

--- ValidParentheses/src/validParentheses/ValidParentheses.java
+++ ValidParentheses/src/validParentheses/ValidParentheses.java
@@ -2,17 +2,17 @@ package validParentheses;
 
 import java.util.LinkedList;
 
-// LeetCode #20
+// LeetCode #20 (Valid Parentheses).
 
-// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-// The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine 
+// if the input string is valid.
+
+// The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and
+// "([)]" are not.
 
 public class ValidParentheses {
 
 	public boolean isValid(String s) {
-		if (s == null || s.length() == 0) {
-			return true;
-		}
 		LinkedList<Character> stack = new LinkedList<>();
 		for (int i = 0; i < s.length(); i++) {
 			if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
