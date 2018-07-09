bccac982823dc0fa4cd152a99b5765bbbf947d08
Fan Feng <fanfeng2015@gmail.com>
2018-07-09 16:28:02 -0700

    LeetCode #125

--- ValidPalindrome/src/validPalindrome/ValidPalindrome.java
+++ ValidPalindrome/src/validPalindrome/ValidPalindrome.java
@@ -1,6 +1,6 @@
 package validPalindrome;
 
-// LeetCode #125
+// LeetCode #125 (Valid Palindrome).
 
 // Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
@@ -15,11 +15,10 @@ public class ValidPalindrome {
 			while (right > left && !Character.isLetterOrDigit(s.charAt(right))) {
 				right--;
 			}
-			if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+			if (Character.toLowerCase(s.charAt(left++)) != Character.toLowerCase(s.charAt(right--))) {
 				return false;
 			}
-			left++;
-			right--;
+
 		}
 		return true;
 	}
