848a18f6c034e355c611a01ddb48eba94f2d3258
Fan Feng <fanfeng2015@gmail.com>
2018-07-10 13:25:45 -0700

    LeetCode #118

--- PascalsTriangle/src/pascalsTriangle/PascalsTriangle.java
+++ PascalsTriangle/src/pascalsTriangle/PascalsTriangle.java
@@ -3,23 +3,28 @@ package pascalsTriangle;
 import java.util.ArrayList;
 import java.util.List;
 
-// LeetCode #118
+// LeetCode #118 (Pascal's Triangle).
+
 // Given numRows, generate the first numRows of Pascal's triangle.
 
 public class PascalsTriangle {
 
 	public List<List<Integer>> generate(int numRows) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
-		List<Integer> cur = new ArrayList<>();
 		if (numRows < 0) {
 			return result;
 		}
+		List<Integer> prev = new ArrayList<>(), cur = new ArrayList<>();
+		prev.add(1);
 		for (int i = 0; i < numRows; i++) {
-			cur.add(0, 1);
-			for (int j = 1; j < cur.size() - 1; j++) {
-				cur.set(j, cur.get(j) + cur.get(j + 1));
+			result.add(new ArrayList<Integer>(prev));
+			cur.add(1);
+			for (int j = 0; j < prev.size() - 1; j++) {
+				cur.add(prev.get(j) + prev.get(j + 1));
 			}
-			result.add(new ArrayList<Integer>(cur));
+			cur.add(1);
+			prev = cur;
+			cur = new ArrayList<>();
 		}
 		return result;
 	}
