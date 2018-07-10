6d90a0ee76acd61e9137fc73cb2a3e0dbb9eeb7a
Fan Feng <fanfeng2015@gmail.com>
2018-07-10 16:42:13 -0700

    LeetCode #126

--- WordLadder2/src/wordLadder2/WordLadder2.java
+++ WordLadder2/src/wordLadder2/WordLadder2.java
@@ -20,7 +20,7 @@ import java.util.Set;
 // Note:
 // 1. Return an empty list if there is no such transformation sequence.
 // 2. All words have the same length.
-// 3. All words contain only lowercase alphabetic characters.
+// 3. All words contain only lower case alphabetic characters.
 // 4. You may assume no duplicates in the word list.
 // 5. You may assume beginWord and endWord are non-empty and are not the same.
 
@@ -28,22 +28,27 @@ public class WordLadder2 {
 
 	// BFS + DFS
 	public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+		Set<String> dictionary = new HashSet<>(wordList);
+		if (!dictionary.contains(endWord)) {
+			return new ArrayList<List<String>>();
+		}
+
 		Set<String> beginSet = new HashSet<String>();
 		Set<String> endSet = new HashSet<String>();
 		beginSet.add(beginWord);
 		endSet.add(endWord);
 
+		// BFS to construct the map
 		Map<String, List<String>> map = new HashMap<String, List<String>>();
-		BFS(new HashSet<>(wordList), beginSet, endSet, map, false);
+		BFS(dictionary, beginSet, endSet, map, true);
 
+		// DFS to construct the result
 		List<String> solution = new ArrayList<String>(Arrays.asList(beginWord));
 		List<List<String>> result = new ArrayList<List<String>>();
 		DFS(beginWord, endWord, map, solution, result);
-
 		return result;
 	}
 
-	// BFS: construct the map
 	private boolean BFS(Set<String> dictionary, Set<String> beginSet, Set<String> endSet, Map<String, List<String>> map,
 			boolean downward) {
 		if (beginSet.isEmpty()) {
@@ -53,12 +58,12 @@ public class WordLadder2 {
 		if (beginSet.size() > endSet.size()) {
 			return BFS(dictionary, endSet, beginSet, map, !downward);
 		}
+
 		dictionary.removeAll(beginSet);
 		dictionary.removeAll(endSet);
 
 		boolean shortestFound = false;
 		Set<String> temp = new HashSet<String>(); // next level of BFS
-
 		// try out all 26 possible characters for each char of each string in beginSet
 		for (String s : beginSet) {
 			char[] chars = s.toCharArray();
@@ -66,18 +71,18 @@ public class WordLadder2 {
 				char originalChar = chars[i];
 				for (char ch = 'a'; ch <= 'z'; ch++) {
 					chars[i] = ch; // modify
-					String word = new String(chars);
+					String nextWord = new String(chars);
 					// check direction
-					String key = downward ? word : s;
-					String value = downward ? s : word;
+					String key = downward ? s : nextWord;
+					String value = downward ? nextWord : s;
 					List<String> list = map.containsKey(key) ? map.get(key) : new ArrayList<String>();
-					if (endSet.contains(word)) {
+					if (endSet.contains(nextWord)) {
 						shortestFound = true;
 						list.add(value);
 						map.put(key, list);
 					}
-					if (!shortestFound && dictionary.contains(word)) { // TODO: visited?
-						temp.add(word);
+					if (!shortestFound && dictionary.contains(nextWord)) {
+						temp.add(nextWord);
 						list.add(value);
 						map.put(key, list);
 					}
@@ -85,7 +90,8 @@ public class WordLadder2 {
 				}
 			}
 		}
-		return shortestFound || BFS(dictionary, beginSet, endSet, map, !downward);
+		beginSet = temp;
+		return shortestFound || BFS(dictionary, endSet, beginSet, map, !downward);
 	}
 
 	private void DFS(String beginWord, String endWord, Map<String, List<String>> map, List<String> solution,
