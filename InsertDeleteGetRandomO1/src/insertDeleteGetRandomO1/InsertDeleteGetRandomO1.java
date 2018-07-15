3a1615cba9979d6c9236d493ce6a7018e3c9d4f4
Fan Feng <fanfeng2015@gmail.com>
2018-07-14 17:17:34 -0700

    LeetCode #297, #449

--- InsertDeleteGetRandomO1/src/insertDeleteGetRandomO1/InsertDeleteGetRandomO1.java
+++ InsertDeleteGetRandomO1/src/insertDeleteGetRandomO1/InsertDeleteGetRandomO1.java
@@ -1,16 +1,19 @@
 package insertDeleteGetRandomO1;
 
-// LeetCode #380
-// Design a data structure that supports all following operations in average O(1) time:
-// insert(val): Inserts an item val to the set if not already present.
-// remove(val): Removes an item val from the set if present.
-// getRandom(): Returns a random element from current set of elements. Each element must have the same probability of being returned.
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+
+// LeetCode #380 (Insert Delete Get Random O(1)).
+
+// Design a data structure that supports all following operations in average O(1) time:
+
+// insert(val): Inserts an item val to the set if not already present.
+// remove(val): Removes an item val from the set if present.
+// getRandom(): Returns a random element from current set of elements. Each element must
+//              have the same probability of being returned.
 
 public class InsertDeleteGetRandomO1 {
 
