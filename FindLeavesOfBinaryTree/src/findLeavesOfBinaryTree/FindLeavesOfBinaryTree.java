4474dd919dcec12686359a09ec1f15b47250de95
Fan Feng <fanfeng2015@gmail.com>
2018-07-16 15:09:57 -0700

    LeetCode #366

--- FindLeavesOfBinaryTree/src/findLeavesOfBinaryTree/FindLeavesOfBinaryTree.java
+++ FindLeavesOfBinaryTree/src/findLeavesOfBinaryTree/FindLeavesOfBinaryTree.java
@@ -3,27 +3,28 @@ package findLeavesOfBinaryTree;
 import java.util.ArrayList;
 import java.util.List;
 
-// LeetCode #366
+// LeetCode #366 (Find Leaves of Binary Tree).
+
 // Given a binary tree, collect a tree's nodes as if you were doing this: 
+
 // Collect and remove all leaves, repeat until the tree is empty.
+
 // For example, given a binary tree [1, 2, 3, 4, 5, #, #], return [[4, 5, 3], [2], [1]].
 
 public class FindLeavesOfBinaryTree {
 
 	public List<List<Integer>> findLeaves(TreeNode root) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
-		helper(root, result);
+		findLeaves(root, result);
 		return result;
 	}
 
-	private int helper(TreeNode root, List<List<Integer>> result) {
-		// base case
+	private int findLeaves(TreeNode root, List<List<Integer>> result) {
 		if (root == null) {
 			return -1;
 		}
-		// recursive rule
-		int left = helper(root.left, result);
-		int right = helper(root.right, result);
+		int left = findLeaves(root.left, result);
+		int right = findLeaves(root.right, result);
 		int height = Math.max(left, right) + 1;
 		if (result.size() < height + 1) {
 			result.add(new ArrayList<Integer>());
@@ -33,6 +34,5 @@ public class FindLeavesOfBinaryTree {
 	}
 
 	// Time complexity is O(n).
-	// Space complexity is O(n), because of call-stack, ignoring space needed
-	// for output.
+	// Space complexity is O(n), when the Fbinary tree is highly unbalanced.
 }
