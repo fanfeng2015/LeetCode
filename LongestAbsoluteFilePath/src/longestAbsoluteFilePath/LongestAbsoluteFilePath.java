93848d7a82e448933b6aa6157c84f69a850468e3
Fan Feng <fanfeng2015@gmail.com>
2018-07-16 21:56:00 -0700

    LeetCode #388

--- LongestAbsoluteFilePath/src/longestAbsoluteFilePath/LongestAbsoluteFilePath.java
+++ LongestAbsoluteFilePath/src/longestAbsoluteFilePath/LongestAbsoluteFilePath.java
@@ -2,19 +2,19 @@ package longestAbsoluteFilePath;
 
 import java.util.LinkedList;
 
-// LeetCode #388
+// LeetCode #388 (Longest Absolute File Path).
 
 public class LongestAbsoluteFilePath {
 
 	// Assumption: neither directory name, nor file name has \t in it.
 	public int lengthLongestPath(String input) {
-		LinkedList<Integer> stack = new LinkedList<>();
+		LinkedList<Integer> stack = new LinkedList<>(); // stores length
 		stack.offerLast(0);
 		int result = 0;
 		for (String s : input.split("\n")) {
 			// \t is treated as a char, or as if it has length 1
 			int level = s.lastIndexOf("\t") + 1; // number of tabs
-			while (level + 1 < stack.size()) { // notice: it is a while loop
+			while (level + 1 < stack.size()) {
 				// another directory/file on a previous level
 				stack.pollLast();
 			}
