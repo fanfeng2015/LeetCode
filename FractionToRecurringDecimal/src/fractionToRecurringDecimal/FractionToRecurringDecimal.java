8a151e1f88b5a4edd06c22d5a9e0d7f413079428
Fan Feng <fanfeng2015@gmail.com>
2018-07-17 09:53:47 -0700

    LeetCode #166

--- FractionToRecurringDecimal/src/fractionToRecurringDecimal/FractionToRecurringDecimal.java
+++ FractionToRecurringDecimal/src/fractionToRecurringDecimal/FractionToRecurringDecimal.java
@@ -3,7 +3,12 @@ package fractionToRecurringDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-// LeetCode #166
+// LeetCode #166 (Fraction to Recurring Decimal).
+
+// Given two integers representing the numerator and denominator of a fraction, 
+// return the fraction in string format.
+
+// If the fractional part is repeating, enclose the repeating part in parentheses.
 
 public class FractionToRecurringDecimal {
 
