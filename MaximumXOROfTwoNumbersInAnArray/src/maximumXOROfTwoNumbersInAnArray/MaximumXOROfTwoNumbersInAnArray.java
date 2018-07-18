f7fc9f1681e6758c08313ce23ab0df233cb64221
Fan Feng <fanfeng2015@gmail.com>
2018-07-18 15:59:02 -0700

    LeetCode #421

--- MaximumXOROfTwoNumbersInAnArray/src/maximumXOROfTwoNumbersInAnArray/MaximumXOROfTwoNumbersInAnArray.java
+++ MaximumXOROfTwoNumbersInAnArray/src/maximumXOROfTwoNumbersInAnArray/MaximumXOROfTwoNumbersInAnArray.java
@@ -3,10 +3,17 @@ package maximumXOROfTwoNumbersInAnArray;
 import java.util.HashSet;
 import java.util.Set;
 
-// LeetCode #421
+// LeetCode #421 (Maximum XOR of Two Numbers in an Array).
+
+// Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 231.
+
+// Find the maximum result of ai XOR aj, where 0 ≤ i, j < n.
+
+// Could you do this in O(n) runtime?
 
 public class MaximumXOROfTwoNumbersInAnArray {
 
+	// Solution 1: Bit manipulation, don't understand...
 	public int findMaximumXOR(int[] nums) {
 		int max = 0, mask = 0;
 		for (int i = 31; i >= 0; i--) {
@@ -15,9 +22,6 @@ public class MaximumXOROfTwoNumbersInAnArray {
 			for (int num : nums) {
 				set.add(num & mask); // only keep bits that are to the left of i
 			}
-			// we want to check if there exist two numbers,
-			// such that XOR of their i-th bit evaluates to 1
-			// fact: if a ^ b = c, then a ^ c = b && b ^ c = a
 			int target = max | (1 << i);
 			for (int num : set) {
 				if (set.contains(target ^ num)) {
@@ -27,6 +31,54 @@ public class MaximumXOROfTwoNumbersInAnArray {
 			}
 		}
 		return max;
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(n).
+
+	// Solution 2: Trie
+	public int findMaximumXOR2(int[] nums) {
+		TrieNode root = initializeTrie(nums);
+		int max = 0;
+		for (int num : nums) {
+			int sum = 0;
+			TrieNode cur = root;
+			for (int i = 31; i >= 0; i--) {
+				// increment sum only if some number has a different bit at this position
+				int bit = (num >>> i) & 1; // unsigned right shift
+				if (cur.children[bit ^ 1] != null) {
+					sum += (1 << i);
+					cur = cur.children[bit ^ 1];
+				} else { // all numbers' current bit is the same
+					cur = cur.children[bit];
+				}
+			}
+			max = Math.max(max, sum);
+		}
+		return max;
+	}
+
+	private TrieNode initializeTrie(int[] nums) {
+		TrieNode root = new TrieNode();
+		for (int num : nums) {
+			TrieNode cur = root;
+			for (int i = 31; i >= 0; i--) {
+				int bit = (num >>> i) & 1; // unsigned right shift
+				if (cur.children[bit] == null) {
+					cur.children[bit] = new TrieNode();
+				}
+				cur = cur.children[bit];
+			}
+		}
+		return root;
+	}
+
+	private class TrieNode {
+		TrieNode[] children;
+
+		TrieNode() {
+			this.children = new TrieNode[2];
+		}
 	}
 
 	// Time complexity is O(n).
