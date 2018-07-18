a11aacd362ce54a5c0f0a084fdfab801cda4b97a
Fan Feng <fanfeng2015@gmail.com>
2018-07-18 00:02:39 -0700

    LeetCode #289

--- GameOfLife/src/gameOfLife/GameOfLife.java
+++ GameOfLife/src/gameOfLife/GameOfLife.java
@@ -1,6 +1,20 @@
 package gameOfLife;
 
-// LeetCode #289
+// LeetCode #289 (Game of Life).
+
+// According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton
+// devised by the British mathematician John Horton Conway in 1970."
+
+// Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with
+// its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+
+// Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+// Any live cell with two or three live neighbors lives on to the next generation.
+// Any live cell with more than three live neighbors dies, as if by over-population..
+// Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+// Write a function to compute the next state (after one update) of the board given its current state. The next state is created
+// by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously.
 
 public class GameOfLife {
 
@@ -15,7 +29,7 @@ public class GameOfLife {
 				int count = 0;
 				for (int[] dir : directions) {
 					int row = i + dir[0], col = j + dir[1];
-					if (row >= 0 && row <= m - 1 && col >= 0 && col <= n - 1) {
+					if (row >= 0 && row < m && col >= 0 && col < n) {
 						count = M[row][col] == 1 ? count + 1 : count;
 					}
 				}
@@ -46,5 +60,4 @@ public class GameOfLife {
 	// Answer: Use the second least bit to store the next state,
 	// In the first pass, compute the next state; in the second pass, shift all
 	// values to the right by one bit.
-
 }
