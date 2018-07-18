c0118b73c595203df86f68264bfdfe7b3ce83d92
Fan Feng <fanfeng2015@gmail.com>
2018-07-17 23:54:41 -0700

    LeetCode #448

--- FindAllNumbersDisappearedInAnArray/src/findAllNumbersDisappearedInAnArray/FindAllNumbersDisappearedInAnArray.java
+++ FindAllNumbersDisappearedInAnArray/src/findAllNumbersDisappearedInAnArray/FindAllNumbersDisappearedInAnArray.java
@@ -3,9 +3,11 @@ package findAllNumbersDisappearedInAnArray;
 import java.util.ArrayList;
 import java.util.List;
 
-// LeetCode #448
+// LeetCode #448 (Find All Numbers Disappeared in an Array).
 
-// Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+// Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some 
+// elements appear twice and others appear once.
+
 // Find all the elements of [1, n] inclusive that do not appear in this array.
 
 public class FindAllNumbersDisappearedInAnArray {
