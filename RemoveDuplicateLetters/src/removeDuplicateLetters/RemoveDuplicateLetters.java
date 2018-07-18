571ac73df0a736f34cfaba78f195cd09b12f80fc
Fan Feng <fanfeng2015@gmail.com>
2018-07-18 11:04:01 -0700

    LeetCode #316

--- RemoveDuplicateLetters/src/removeDuplicateLetters/RemoveDuplicateLetters.java
+++ RemoveDuplicateLetters/src/removeDuplicateLetters/RemoveDuplicateLetters.java
@@ -1,36 +1,44 @@
 package removeDuplicateLetters;
 
-// LeetCode #316
+// LeetCode #316 (Remove Duplicate Letters).
 
-// Given a string which contains only lower-case letters, 
-// remove duplicate letters so that every letter appear once and only once. 
-// You must make sure your result is the smallest in lexicographical order 
-// among all possible results.
+// Given a string which contains only lower-case letters, remove duplicate letters
+// so that every letter appears once and only once. 
+
+// You must make sure your result is the smallest in lexicographical order among all
+// possible results.
 
 public class RemoveDuplicateLetters {
 
+	private static final int NUM_LETTERS = 26;
+
 	public String removeDuplicateLetters(String s) {
-		int[] count = new int[26];
-		boolean[] visited = new boolean[26];
-		char[] chars = s.toCharArray();
-		for (char ch : chars) {
-			count[ch - 'a']++;
-		}
+		int[] countMap = countFrequency(s);
+		boolean[] visited = new boolean[NUM_LETTERS];
 		StringBuilder sb = new StringBuilder();
-		for (char ch : chars) {
-			int index = ch - 'a';
-			count[index]--;
-			if (!visited[index]) {
-				while (sb.length() > 0 && ch < sb.charAt(sb.length() - 1)
-						&& count[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+		for (char ch : s.toCharArray()) {
+			countMap[ch - 'a']--;
+			if (!visited[ch - 'a']) {
+				// keep removing the last char as long the it is greater than the current char
+				// and is not the last occurrence
+				while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch
+						&& countMap[sb.charAt(sb.length() - 1) - 'a'] > 0) {
 					visited[sb.charAt(sb.length() - 1) - 'a'] = false;
 					sb.deleteCharAt(sb.length() - 1);
 				}
 				sb.append(ch);
-				visited[index] = true;
+				visited[ch - 'a'] = true;
 			}
 		}
 		return sb.toString();
+	}
+
+	private int[] countFrequency(String s) {
+		int[] countMap = new int[NUM_LETTERS];
+		for (char ch : s.toCharArray()) {
+			countMap[ch - 'a']++;
+		}
+		return countMap;
 	}
 
 	// Time complexity is O(n).
