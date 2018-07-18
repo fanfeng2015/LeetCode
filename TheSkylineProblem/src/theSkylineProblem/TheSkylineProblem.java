ffd963a03a325fdbb92de2ccb38642ff3c193985
Fan Feng <fanfeng2015@gmail.com>
2018-07-18 12:04:21 -0700

    LeetCode #218

--- TheSkylineProblem/src/theSkylineProblem/TheSkylineProblem.java
+++ TheSkylineProblem/src/theSkylineProblem/TheSkylineProblem.java
@@ -2,43 +2,32 @@ package theSkylineProblem;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
-// LeetCode #218
+// LeetCode #218 (The Skyline Problem).
 
 public class TheSkylineProblem {
 
 	public List<int[]> getSkyline(int[][] buildings) {
-		List<int[]> result = new ArrayList<>();
 		List<int[]> heights = new ArrayList<>();
 		for (int[] building : buildings) {
 			heights.add(new int[] { building[0], -building[2] });
 			heights.add(new int[] { building[1], building[2] });
 		}
-		Collections.sort(heights, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] one, int[] two) {
-				if (one[0] != two[0]) {
-					return one[0] < two[0] ? -1 : 1;
-				} else if (one[1] != two[1]) {
-					return one[1] < two[1] ? -1 : 1;
-				}
-				return 0;
+		// lambda expression
+		Collections.sort(heights, (a, b) -> {
+			if (a[0] != b[0]) {
+				return a[0] - b[0];
 			}
+			return a[1] - b[1];
 		});
-		PriorityQueue<Integer> maxHeap = new PriorityQueue<>(heights.size(), new Comparator<Integer>() {
-			@Override
-			public int compare(Integer x, Integer y) {
-				if (x == y) {
-					return 0;
-				}
-				return x > y ? -1 : 1;
-			}
-		});
-		maxHeap.offer(0);
+
 		int prev = 0;
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>(heights.size(), (a, b) -> (b - a));
+		maxHeap.offer(0);
+
+		List<int[]> result = new ArrayList<>();
 		for (int[] height : heights) {
 			if (height[1] < 0) {
 				maxHeap.offer(-height[1]);
@@ -54,7 +43,7 @@ public class TheSkylineProblem {
 		return result;
 	}
 
-	// Time complexity is O(n^2), because remove from a priority queue takes
-	// O(n) time.
+	// Time complexity is O(n^2), because remove from a priority queue takes O(n)
+	// time.
 	// Space complexity is O(n).
 }
