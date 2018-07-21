af7a9bdd685446cae8e1e902e72b23dce28482d2
Fan Feng <fanfeng2015@gmail.com>
2018-07-20 17:42:46 -0700

    LeetCode #283

---
+++ MoveZeroes/src/moveZeroes/MoveZeroes.java
@@ -0,0 +1,26 @@
+package moveZeroes;
+
+import java.util.Arrays;
+
+// LeetCode #283 (Move Zeroes).
+
+// Given an array of integers, move all the 0s to the right end of the array.
+
+// The relative order of the elements in the original array need to be maintained.
+
+public class MoveZeroes {
+
+	public int[] moveZero(int[] array) {
+		int left = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != 0) {
+				array[left++] = array[i];
+			}
+		}
+		Arrays.fill(array, left, array.length, 0);
+		return array;
+	}
+
+	// Time complexity is O(n).
+	// Space complexity is O(1).
+}
