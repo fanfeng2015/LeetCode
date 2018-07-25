5dc0c9527a279c4b704512524de957e741e7f332
Fan Feng <fanfeng2015@gmail.com>
2018-07-25 14:14:20 -0700

    LeetCode #108

--- ConvertSortedArrayToBST/src/convertSortedArrayToBST/ConvertSortedArrayToBST.java
+++ ConvertSortedArrayToBST/src/convertSortedArrayToBST/ConvertSortedArrayToBST.java
@@ -1,6 +1,6 @@
 package convertSortedArrayToBST;
 
-// LeetCode #108
+// LeetCode #108 (Convert Sorted Array to BST).
 
 // Given an array where elements are sorted in ascending order, 
 // convert it to a height balanced BST.
@@ -8,18 +8,13 @@ package convertSortedArrayToBST;
 public class ConvertSortedArrayToBST {
 
 	public TreeNode sortedArrayToBST(int[] nums) {
-		if (nums == null || nums.length == 0) {
-			return null;
-		}
 		return sortedArrayToBST(nums, 0, nums.length - 1);
 	}
 
 	private TreeNode sortedArrayToBST(int[] nums, int left, int right) {
-		// base case
 		if (left > right) {
 			return null;
 		}
-		// recursive rule
 		int mid = left + (right - left) / 2;
 		TreeNode root = new TreeNode(nums[mid]);
 		root.left = sortedArrayToBST(nums, left, mid - 1);
