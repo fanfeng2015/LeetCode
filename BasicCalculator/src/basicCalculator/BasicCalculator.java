9e3a114924b8dd8228ee0eb4115c737441e781f6
Fan Feng <fanfeng2015@gmail.com>
2018-07-26 11:34:49 -0700

    LeetCode #224

--- BasicCalculator/src/basicCalculator/BasicCalculator.java
+++ BasicCalculator/src/basicCalculator/BasicCalculator.java
@@ -2,7 +2,7 @@ package basicCalculator;
 
 import java.util.LinkedList;
 
-// LeetCode #224
+// LeetCode #224 (Basic Calculator).
 
 // Implement a basic calculator to evaluate a simple expression string.
 // The expression string may contain open parentheses ( and closing parentheses ), 
@@ -13,15 +13,16 @@ import java.util.LinkedList;
 public class BasicCalculator {
 
 	public int calculate(String s) {
+		int i = 0;
 		int result = 0, sign = 1;
 		LinkedList<Integer> stack = new LinkedList<>();
-		for (int i = 0; i < s.length(); i++) {
+		while (i < s.length()) {
 			if (Character.isDigit(s.charAt(i))) {
 				int start = i;
 				while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
 					i++;
 				}
-				result += Integer.parseInt(s.substring(start, i)) * sign;
+				result += Integer.parseInt(s.substring(start, i + 1)) * sign;
 			} else if (s.charAt(i) == '+') {
 				sign = 1;
 			} else if (s.charAt(i) == '-') {
@@ -34,6 +35,7 @@ public class BasicCalculator {
 			} else if (s.charAt(i) == ')') {
 				result = result * stack.pollLast() + stack.pollLast();
 			}
+			i++;
 		}
 		return result;
 	}
