18567e573fb3c01dff3a9f5dac12e83a7faa520a
Fan Feng <fanfeng2015@gmail.com>
2018-07-27 13:18:29 -0700

    LeetCode #104

--- MaximumDepthOfBinaryTree/src/maximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.java
+++ MaximumDepthOfBinaryTree/src/maximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.java
@@ -1,24 +1,21 @@
 package maximumDepthOfBinaryTree;
 
-// LeetCode #104
+// LeetCode #104 (Maximum Depth of Binary Tree).
+
 // Given a binary tree, find its maximum depth.
-// The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// The maximum depth is the number of nodes along the longest path from the 
+// root node down to the farthest leaf node.
 
 public class MaximumDepthOfBinaryTree {
 
 	public int maxDepth(TreeNode root) {
-		// base case
 		if (root == null) {
 			return 0;
 		}
-		// root != null
-		int left = maxDepth(root.left);
-		int right = maxDepth(root.right);
-		return Math.max(left, right) + 1;
+		return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 	}
 
 	// Time complexity is O(n).
 	// Space complexity is O(n).
-
-	// Solution 2: BFS...
 }
