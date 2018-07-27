525875faaa2b46c5eb0823c7ed635fa4b8b66d05
Fan Feng <fanfeng2015@gmail.com>
2018-07-26 17:49:00 -0700

    LeetCode #210

--- CourseSchedule2/src/courseSchedule2/CourseSchedule2.java
+++ CourseSchedule2/src/courseSchedule2/CourseSchedule2.java
@@ -4,48 +4,62 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-// LeetCode #210
+// LeetCode #210 (Course Schedule II).
+
+// There are a total of n courses you have to take, labeled from 0 to n-1.
+
+// Some courses may have prerequisites, for example to take course 0 you have
+// to first take course 1, which is expressed as a pair [0, 1].
+
+// Given the total number of courses and a list of prerequisite pairs, return 
+// the ordering of courses you should take to finish all courses.
+
+// There may be multiple correct orders,you just need to return one of them.
+// If it is impossible to finish all courses,return an empty array.
 
 public class CourseSchedule2 {
 
 	public int[] findOrder(int numCourses, int[][] prerequisites) {
-		int[] incomingCount = new int[numCourses];
+		int[] incomingDegrees = new int[numCourses];
 		List<List<Integer>> adjacentList = new ArrayList<>();
-		constructGraph(prerequisites, incomingCount, adjacentList);
-		return topologicalOrder(incomingCount, adjacentList);
+		constructGraph(prerequisites, incomingDegrees, adjacentList);
+		return topologicalOrder(incomingDegrees, adjacentList);
 	}
 
-	private void constructGraph(int[][] prerequisites, int[] incomingCount, List<List<Integer>> adjacentList) {
-		int n = incomingCount.length;
-		for (int i = 0; i < n; i++) {
+	private void constructGraph(int[][] prerequisites, int[] incomingDegrees, List<List<Integer>> adjacentList) {
+		int numCourses = incomingDegrees.length;
+		for (int i = 0; i < numCourses; i++) {
 			adjacentList.add(new ArrayList<Integer>());
 		}
 		for (int[] prereq : prerequisites) {
-			incomingCount[prereq[0]]++;
+			incomingDegrees[prereq[0]]++;
 			adjacentList.get(prereq[1]).add(prereq[0]);
 		}
 	}
 
-	private int[] topologicalOrder(int[] incomingCount, List<List<Integer>> adjacentList) {
-		int n = incomingCount.length;
-		int[] result = new int[n];
+	private int[] topologicalOrder(int[] incomingDegrees, List<List<Integer>> adjacentList) {
+		int numCourses = incomingDegrees.length;
 		LinkedList<Integer> queue = new LinkedList<>();
-		for (int i = 0; i < n; i++) {
-			if (incomingCount[i] == 0) {
+		for (int i = 0; i < numCourses; i++) {
+			if (incomingDegrees[i] == 0) {
 				queue.offerFirst(i);
 			}
 		}
 		int index = 0;
+		int[] result = new int[numCourses];
 		while (!queue.isEmpty()) {
 			int cur = queue.pollLast();
 			result[index++] = cur;
-			for (int next : adjacentList.get(cur)) {
-				incomingCount[next]--;
-				if (incomingCount[next] == 0) {
-					queue.offerFirst(next);
+			for (int neighbor : adjacentList.get(cur)) {
+				incomingDegrees[neighbor]--;
+				if (incomingDegrees[neighbor] == 0) {
+					queue.offerFirst(neighbor);
 				}
 			}
 		}
-		return index == n ? result : new int[] {};
+		return index == numCourses ? result : new int[] {};
 	}
+
+	// Time complexity is O(n), where n is the number of prerequisites.
+	// Space complexity is O(n).
 }
