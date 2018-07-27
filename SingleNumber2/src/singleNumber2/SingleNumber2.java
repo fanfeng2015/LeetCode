bce649d06461dfabd98a2fbf9f2bd6ba7e1dd65a
Fan Feng <fanfeng2015@gmail.com>
2018-07-27 13:44:28 -0700

    LeetCode #137

--- SingleNumber2/src/singleNumber2/SingleNumber2.java
+++ SingleNumber2/src/singleNumber2/SingleNumber2.java
@@ -1,15 +1,13 @@
 package singleNumber2;
 
-// LeetCode #137
+// LeetCode #137 (Single Number II).
 
-// Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+// Given an array of integers, every element appears three times except for one, which
+// appears exactly once. Find that single one.
 
 public class SingleNumber2 {
 
 	public int singleNumber(int[] nums) {
-		// use two bits to count the total number of bits that are 1 (00, 01,
-		// 10), ones keep all first bits of them, twos keep all second bits of
-		// them
 		int ones = 0, twos = 0;
 		for (int i : nums) {
 			ones = (ones ^ i) & ~twos;
