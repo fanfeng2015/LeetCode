bf8354f8ad1dd4c7b0aad227a81289c082bbf761
Fan Feng <fanfeng2015@gmail.com>
2018-07-29 14:47:00 -0700

    LeetCode #14

--- LongestCommonPrefix/src/longestCommonPrefix/LongestCommonPrefix.java
+++ LongestCommonPrefix/src/longestCommonPrefix/LongestCommonPrefix.java
@@ -2,14 +2,14 @@ package longestCommonPrefix;
 
 import java.util.Arrays;
 
-// LeetCode #14
+// LeetCode #14 (Longest Common Prefix).
 
 // Write a function to find the longest common prefix string amongst an array of strings.
 
 public class LongestCommonPrefix {
 
-	// Solution 1: find the shortest String, and check for existence of each
-	// of its character in all other words
+	// find the shortest String, and check for existence of each of its character in
+	// all other words
 	public String longestCommonPrefix(String[] strs) {
 		if (strs == null || strs.length == 0) {
 			return "";
@@ -18,15 +18,15 @@ public class LongestCommonPrefix {
 		int index = 0, min = Integer.MAX_VALUE;
 		for (int i = 0; i < strs.length; i++) {
 			if (strs[i].length() < min) {
-				min = strs[i].length();
 				index = i;
+				min = strs[i].length();
 			}
 		}
 		String shortest = strs[index];
-		for (index = 0; index < shortest.length(); index++) {
+		for (int i = 0; i < shortest.length(); i++) {
 			for (int j = 0; j < strs.length; j++) {
-				if (strs[j].charAt(index) != shortest.charAt(index)) {
-					return shortest.substring(0, index);
+				if (strs[j].charAt(i) != shortest.charAt(i)) {
+					return shortest.substring(0, i);
 				}
 			}
 		}
@@ -36,17 +36,15 @@ public class LongestCommonPrefix {
 	// Time complexity is O(n * k).
 	// Space complexity is O(1).
 
-	// Solution 2: sort first, then check for common prefix of the first and
-	// last String
+	// sort first, then check for common prefix of the first an last string
 	public String longestCommonPrefix2(String[] strs) {
 		if (strs == null || strs.length == 0) {
 			return "";
 		}
 		Arrays.sort(strs);
-		char[] first = strs[0].toCharArray();
-		char[] last = strs[strs.length - 1].toCharArray();
+		String first = strs[0], last = strs[strs.length - 1];
 		int index = 0;
-		while (index < first.length && index < last.length && first[index] == last[index]) {
+		while (index < first.length() && index < last.length() && first.charAt(index) == last.charAt(index)) {
 			index++;
 		}
 		return strs[0].substring(0, index);
