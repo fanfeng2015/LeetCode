615f4e0899197c2dfe0e8e954169b3a17d5f26eb
Fan Feng <fanfeng2015@gmail.com>
2018-07-30 16:19:20 -0700

    LeetCode #280

--- WiggleSort/src/wiggleSort/WiggleSort.java
+++ WiggleSort/src/wiggleSort/WiggleSort.java
@@ -1,26 +1,28 @@
 package wiggleSort;
 
-// LeetCode #280
+// LeetCode #280 (Wiggle Sort).
 
 // Given an unsorted array nums, reorder it in-place such that 
-// nums[0] <= nums[1] >= nums[2] <= nums[3]....
+// nums[0] <= nums[1] >= nums[2] <= nums[3] >= ...
 
 public class WiggleSort {
 
-	public void wiggleSort(int[] nums) {
-		for (int i = 0; i < nums.length - 1; i++) {
-			if (i % 2 == 0 && nums[i] > nums[i + 1]) {
-				swap(nums, i, i + 1);
-			} else if (i % 2 == 1 && nums[i] < nums[i + 1]) {
-				swap(nums, i, i + 1);
+	// one pass linear time
+	public void wiggleSort(int[] array) {
+		if (array == null || array.length <= 1) {
+			return;
+		}
+		for (int i = 0; i < array.length - 1; i++) {
+			if ((i % 2 == 0 && array[i] > array[i + 1]) || (i % 2 == 1 && array[i] < array[i + 1])) {
+				swap(array, i, i + 1);
 			}
 		}
 	}
 
-	private void swap(int[] nums, int i, int j) {
-		int temp = nums[i];
-		nums[i] = nums[j];
-		nums[j] = temp;
+	private void swap(int[] array, int left, int right) {
+		int temp = array[left];
+		array[left] = array[right];
+		array[right] = temp;
 	}
 
 	// Time complexity is O(n).
