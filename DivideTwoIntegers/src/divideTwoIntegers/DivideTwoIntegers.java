cb511dd7d7d3d59aea6e031d0784b9711c3b61bc
Fan Feng <fanfeng2015@gmail.com>
2018-07-30 16:10:33 -0700

    LeetCode #29

--- DivideTwoIntegers/src/divideTwoIntegers/DivideTwoIntegers.java
+++ DivideTwoIntegers/src/divideTwoIntegers/DivideTwoIntegers.java
@@ -1,9 +1,10 @@
 package divideTwoIntegers;
 
-// LeetCode #29
+// LeetCode #29 (Divide Two Integer).
 
 // Divide two integers without using multiplication, division and mod operator.
-// If it is overflow, return MAX_INT
+
+// If it is overflow, return MAX_INT.
 
 public class DivideTwoIntegers {
 
