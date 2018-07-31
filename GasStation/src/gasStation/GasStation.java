ff8840019805895261644fe46052e909eda6a766
Fan Feng <fanfeng2015@gmail.com>
2018-07-30 21:44:44 -0700

    LeetCode #134

--- GasStation/src/gasStation/GasStation.java
+++ GasStation/src/gasStation/GasStation.java
@@ -1,6 +1,6 @@
 package gasStation;
 
-// LeetCode #134
+// LeetCode #134 (Gas Station).
 
 // There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
 // You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). 
@@ -12,15 +12,14 @@ package gasStation;
 public class GasStation {
 
 	// Fact 1: If a car can not reach station B from station A, then it can not
-	// reach station B, from any station at [A, B).
+	// reach station B from any station in [A, B), and from B it can not reach B+1.
 	// Fact 2: If the total number of gas is greater than the total number of
 	// cost, there must exist a solution.
-
 	public int canCompleteCircuit(int[] gas, int[] cost) {
 		int start = 0, gasInTank = 0, sum = 0;
 		for (int i = 0; i < gas.length; i++) {
-			gasInTank += gas[i] - cost[i];
-			if (gasInTank < 0) {
+			gasInTank += gas[i] - cost[i]; // cumulative gas in [start, i]
+			if (gasInTank < 0) { // can not reach i from start
 				start = i + 1;
 				sum += gasInTank;
 				gasInTank = 0;
