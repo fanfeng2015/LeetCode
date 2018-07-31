ca9659ec2b15f5002602abe6456e1744c73ee9a8
Fan Feng <fanfeng2015@gmail.com>
2018-07-31 15:08:54 -0700

    LeetCode #224

--- BasicCalculator/src/basicCalculator/BasicCalculator.java
+++ BasicCalculator/src/basicCalculator/BasicCalculator.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 // LeetCode #224 (Basic Calculator).
 
 // Implement a basic calculator to evaluate a simple expression string.
+
 // The expression string may contain open parentheses ( and closing parentheses ), 
 // the plus + or minus sign -, non-negative integers and empty spaces.
 
@@ -13,29 +14,30 @@ import java.util.LinkedList;
 public class BasicCalculator {
 
 	public int calculate(String s) {
-		int i = 0;
-		int result = 0, sign = 1;
+		int result = 0;
+		int sign = 1, num = 0; // num is the current number between any two operators
 		LinkedList<Integer> stack = new LinkedList<>();
-		while (i < s.length()) {
+		for (int i = 0; i < s.length(); i++) {
 			if (Character.isDigit(s.charAt(i))) {
-				int start = i;
-				while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
-					i++;
-				}
-				result += Integer.parseInt(s.substring(start, i + 1)) * sign;
+				num = 10 * num + Character.getNumericValue(s.charAt(i));
 			} else if (s.charAt(i) == '+') {
+				result += sign * num;
 				sign = 1;
+				num = 0;
 			} else if (s.charAt(i) == '-') {
-				sign = -1;
-			} else if (s.charAt(i) == '(') {
+				result += sign * num;
+				sign = 1;
+				num = 0;
+			} else if (s.charAt(i) == '(') { // num must equal 0
 				stack.offerLast(result);
 				stack.offerLast(sign);
 				result = 0;
 				sign = 1;
 			} else if (s.charAt(i) == ')') {
-				result = result * stack.pollLast() + stack.pollLast();
+				result = (result + sign * num) * stack.pollLast() + stack.pollLast();
+				sign = 1;
+				num = 0;
 			}
-			i++;
 		}
 		return result;
 	}
