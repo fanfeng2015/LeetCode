057edf9bcee40da4ef4f270a6bf1529591687977
Fan Feng <fanfeng2015@gmail.com>
2018-07-30 17:37:26 -0700

    LeetCode #37

--- SudokuSolver/src/sudokuSolver/SudokuSolver.java
+++ SudokuSolver/src/sudokuSolver/SudokuSolver.java
@@ -1,9 +1,11 @@
 package sudokuSolver;
 
-// LeetCode #37
+// LeetCode #37 (Sudoku Solver).
 
 // Write a program to solve a Sudoku puzzle by filling the empty cells.
+
 // Empty cells are indicated by the character '.'.
+
 // You may assume that there will be only one unique solution.
 
 public class SudokuSolver {
@@ -13,10 +15,10 @@ public class SudokuSolver {
 	}
 
 	private boolean DFS(char[][] board) {
-		int n = board.length;
+		int n = board.length; // 9
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (board[i][j] == '.') {
+				if (board[i][j] == '.') { // once the first '.' is found, true/false is always returned
 					for (char ch = '1'; ch <= '9'; ch++) {
 						if (valid(board, i, j, ch)) {
 							board[i][j] = ch;
@@ -34,7 +36,6 @@ public class SudokuSolver {
 	}
 
 	private boolean valid(char[][] board, int i, int j, char ch) {
-		// need to check row i, col j, and square []
 		for (int k = 0; k < board.length; k++) {
 			if (board[i][k] == ch || board[k][j] == ch || board[(i / 3) * 3 + k / 3][(j / 3) * 3 + k % 3] == ch) {
 				return false;
