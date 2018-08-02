51a61a3b3c5d78a66285f80ae29544d1e726d850
Fan Feng <fanfeng2015@gmail.com>
2018-08-02 10:22:43 -0700

    LeetCode #319

--- BulbSwitcher/src/bulbSwitcher/BulbSwitcher.java
+++ BulbSwitcher/src/bulbSwitcher/BulbSwitcher.java
@@ -1,13 +1,14 @@
 package bulbSwitcher;
 
-// LeetCode #319
+// LeetCode #319 (Bulb Switcher).
 
 // There are n bulbs that are initially off. 
+
 // You first turn on all the bulbs. 
 // Then, you turn off every second bulb. 
 // On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). 
-// For the ith round, you toggle every i bulb. 
-// For the nth round, you only toggle the last bulb. 
+// For the i-th round, you toggle every i bulb. 
+// For the n-th round, you only toggle the last bulb. 
 
 // Find how many bulbs are on after n rounds.
 
